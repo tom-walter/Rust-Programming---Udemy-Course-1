c495b7a52d1b78d10753583026eff11a49b20c6b
Tom Walter <tom.k.walter@gmail.com>
2023-08-16 11:44:09 +0200

    Commit Notebook and Files on Section 12: Testing & Documentation

---
+++ phrases/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"lib":{"doc":"","t":"AAAAFFFFFF","n":["greetings","english","french","german","goodbye","hello","goodbye","hello","goodbye","hello"],"q":[[0,"lib"],[1,"lib::greetings"],[4,"lib::greetings::english"],[6,"lib::greetings::french"],[8,"lib::greetings::german"]],"d":["","","","","This function returns the English farewell","This function returns the English greeting","This function returns the French farewell","This function returns the French greeting","This function returns the German farewell","This function returns the German greeting"],"i":[0,0,0,0,0,0,0,0,0,0],"f":[0,0,0,0,[[],1],[[],1],[[],1],[[],1],[[],1],[[],1]],"c":[],"p":[[3,"String"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- phrases/src/lib.rs
+++ phrases/src/lib.rs
@@ -2,19 +2,25 @@ pub mod greetings
 {
     pub mod english
     {
+        /// This function returns the English greeting
         pub fn hello() -> String {"hello".to_string()}
+        /// This function returns the English farewell
         pub fn goodbye() -> String {"goodbye".to_string()}
     }
 
     pub mod french
     {
+        /// This function returns the French greeting
         pub fn hello() -> String {"bonjour".to_string()}
+        /// This function returns the French farewell
         pub fn goodbye() -> String {"au revoir".to_string()}
     }
 
     pub mod german
     {
+        /// This function returns the German greeting
         pub fn hello() -> String {"hallo".to_string()}
+        /// This function returns the German farewell
         pub fn goodbye() -> String {"tschuess".to_string()}
     }
 }
